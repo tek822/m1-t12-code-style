15cbd501566f52a0bfe9943a90b87c1f877137f7
Gusev Roman <tek822@yandex.ru>
2022-11-01 04:29:13 +0300

    style: codestyle fix

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class DepositCalculator {
 
-    double calculateComplexPercent(double a, double y, int d) {
-        double pay = a * Math.pow((1 + y / 12), 12 * d);
+    double calculateComplexPercent(double amount, double yearRate, int depositPeriod) {
+        double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
         return round(pay, 2);
     }
 
@@ -19,16 +19,17 @@ public class DepositCalculator {
     void calculateProfit() {
         int period;
         int action;
+        int amount;
+        double profit=0;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сумму вклада в рублях:");
-        int amount = scanner.nextInt();
+        amount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:");
         period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
 
-        double profit = 0;
         if (action == 1) {
             profit = calculateSimplePercent(amount, 0.06, period);
         } else if (action == 2) {
